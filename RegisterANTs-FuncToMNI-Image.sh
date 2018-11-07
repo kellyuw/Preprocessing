58b93479697944f1a988fa9a9c9a13737c7174d1
Kelly Sambrook <kelly89@uw.edu>
2018-11-07 10:05:02 -0800

    Fixing T1_REG_TYPE option

--- RegisterANTs-FuncToMNI-Image.sh
+++ RegisterANTs-FuncToMNI-Image.sh
@@ -46,7 +46,7 @@ echo ${SUBJECTDIR} ${SUBJECT}
 pwd
 
 echo "Warping ${IMAGE} to MNI"
-if [[ ${T1_REG_TYPE} == *_r* ]]; then
+if [[ ${T1_REG_TYPE} == *r* ]]; then
 	${ANTSpat}/antsApplyTransforms -i ${IMAGE} -r ${MNI_BRAIN_MASK} -t ${MNI_REG_PREFIX}_1Warp.nii.gz ${MNI_REG_PREFIX}_0GenericAffine.mat ${CUSTOM_REG_PREFIX}_1Warp.nii.gz ${CUSTOM_REG_PREFIX}_0GenericAffine.mat ${T1_REG_PREFIX}_0GenericAffine.mat -o ${OUTPUT}
 else
 	${ANTSpath}/antsApplyTransforms -i ${IMAGE} -r ${MNI_BRAIN_MASK} -t ${MNI_REG_PREFIX}_1Warp.nii.gz ${MNI_REG_PREFIX}_0GenericAffine.mat ${CUSTOM_REG_PREFIX}_1Warp.nii.gz ${CUSTOM_REG_PREFIX}_0GenericAffine.mat ${T1_REG_PREFIX}_1Warp.nii.gz ${T1_REG_PREFIX}_0GenericAffine.mat -o ${OUTPUT}
